cd821f54d9a41ccdafc497d67df071c74b30029f
JK <slickerglitch@gmail.com>
2022-09-16 09:57:43 -0500

    Reverted branch 01

---
+++ Game.js
@@ -0,0 +1,37 @@
+// This file will house the Game constructor
+
+$(function () {
+  
+  // start a word find game
+  var gamePuzzle = wordfindgame.create(words, '#puzzle', '#words', {
+    height: 8,
+    width: 15,
+    fillBlanks: true,
+  });
+
+  // create just a puzzle, without filling in the blanks and print to console
+  var puzzle = wordfind.newPuzzle(words, {
+    height: 5,
+    width: 15,
+    fillBlanks: true,
+  });
+  wordfind.print(puzzle);
+});
+
+const Game = function () { 
+
+  this.gamePuzzle = wordfindgame.create(words, '#puzzle', '#words', {
+    height: 8,
+    width: 15,
+    fillBlanks: true,
+  });
+
+  this.puzzle = wordfind.newPuzzle(words, {
+    height: 5,
+    width: 15,
+    fillBlanks: true,
+  });
+
+  wordfind.print(puzzle);
+
+};
